6665e6790de02ea8a5deffa2ec64572f103d98e0
migueberumenjr <migueberumenjr@gmail.com>
2016-08-24 12:48:35 -0700

    cleanup

---
+++ stage1.py
@@ -0,0 +1,8 @@
+from json
+from requests
+
+'''
+This script will POST my JSON object to the registration endpoint for the API challenge
+'''
+regObj = {"token": "a8b4348a10cd839fd0556ed099107e96","github": "https://github.com/migueberumenjr/code2040-TechAssessment/tree/migueberumenjr-step1-1"}
+regAPI = requests.post("http://challenge.code2040.org/api/register", data=json.dumps(regObj))

31d51df7e878b798896fbdd7cdfa7e41e7847dac
migueberumenjr <migueberumenjr@gmail.com>
2016-08-24 12:04:58 -0700

    update whitespace and tabs

--- code2040TechAssessment.py
+++ code2040TechAssessment.py
@@ -3,9 +3,8 @@ import json
 
 def reg():
   '''
-	This function will POST my JSON object to the registration endpoint for the API challenge
-	'''
-	regObj = {"token": "a8b4348a10cd839fd0556ed099107e96","github": "https://github.com/migueberumenjr/code2040-TechAssessment/tree/migueberumenjr-step1-1"}
-	regAPI = requests.post("http://challenge.code2040.org/api/register", data=json.dumps(regObj))
-  
+  This function will POST my JSON object to the registration endpoint for the API challenge
+  '''
+  regObj = {"token": "a8b4348a10cd839fd0556ed099107e96","github": "https://github.com/migueberumenjr/code2040-TechAssessment/tree/migueberumenjr-step1-1"}
+  regAPI = requests.post("http://challenge.code2040.org/api/register", data=json.dumps(regObj))
   return

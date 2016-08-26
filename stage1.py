ebf0fcff5b81eede0001eb1d4cb0bf4568eee9b2
migueberumenjr <migueberumenjr@gmail.com>
2016-08-26 07:57:39 -0700

    added print statement

--- stage1.py
+++ stage1.py
@@ -6,3 +6,5 @@ This script will POST my JSON object to the registration endpoint for the API ch
 '''
 regObj = {"token": "a8b4348a10cd839fd0556ed099107e96","github": "https://github.com/migueberumenjr/code2040-TechAssessment/tree/migueberumenjr-step1-1"}
 regAPI = requests.post("http://challenge.code2040.org/api/register", data=json.dumps(regObj))
+
+print(regAPI.text)

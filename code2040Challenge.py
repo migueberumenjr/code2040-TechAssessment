a884137a24b9c0ea3d061b95d611a5d2d8d38d1f
migueberumenjr <migueberumenjr@gmail.com>
2016-08-26 09:05:59 -0700

    Stage/Step 2 Complete

--- code2040Challenge.py
+++ code2040Challenge.py
@@ -9,8 +9,23 @@ def stage1():
     regObj = {"token": token,"github": "https://github.com/migueberumenjr/code2040-TechAssessment/blob/migueberumenjr-step1-1/"}
     regAPI = requests.post("http://challenge.code2040.org/api/register", json=regObj)
     print(regAPI.text)
+    return token
+
+def stage2(token):
+    tokDic = {"token": token}
+    strRes = requests.post("http://challenge.code2040.org/api/reverse", json=tokDic)
+
+    print(strRes.text)
+    strText = strRes.text
+    revStr = strText[::-1]
+    print(revStr)
+
+    strDic = {"token": token, "string": revStr}
+    strVal = requests.post("http://challenge.code2040.org/api/reverse/validate", json=strDic)
+    print(strVal.text)
 
 def main():
-    stage1()
+    token = stage1()
+    stage2(token)
 
 main()
